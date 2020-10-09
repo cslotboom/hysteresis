64f02ae24fdd39c46d541d30c6b06361d69e394d
cslotboom <50510141+cslotboom@users.noreply.github.com>
2020-10-08 21:31:38 -0700

    Uploading images because fuck it

---
+++ examples/Test Hystresis/Hys1.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  4 22:19:58 2020
+
+@author: Christian
+"""
+
+
+import numpy as np
+import hysteresis as hys
+import time
+import matplotlib.pyplot as plt
+
+
+InputName = 'Hys1.csv'
+LoadProtocolName = 'LoadProtocol.csv'
+EnergyName = 'Hys1_Energy.csv'
+
+xyData          = np.loadtxt(InputName,         delimiter=',')
+LoadProtocol    = np.loadtxt(LoadProtocolName,  delimiter=',',skiprows = 1)
+Energy   = np.loadtxt(EnergyName,  delimiter=',')
+
+xyData[:,1] = xyData[:,1]/1000
+Energy[:,1] = Energy[:,1]
+
+
+
+
+hysTrace = hys.Hysteresis(xyData)
+# hysTrace.plotLoadProtocol()
+# hysTrace.plot(True)
+
+# print(hysTrace.loadProtocol)
+# print(LoadProtocol[:,1])
+
+hysProt = hysTrace.loadProtocol
+cycles = hysTrace.Cycles
+
+FullHys = hys.exandHysTrace(hysTrace, LoadProtocol[:,2], skipStart = 1, skipFailure = True)
+FullHys.plot(True)
+# FullHys.plotLoadProtocol(comparisonProtocol = LoadProtocol[:,1])
+
+
+cumulativex = FullHys.getCumDisp()
+netDisp = FullHys.getNetCumDisp()
+cumulativeArea = FullHys.getCumArea()
+
+
+
+
+
+
+fig, ax = FullHys.plotCumArea(True)
+ax.plot(Energy[:,0],Energy[:,1])
+ax.set_xlabel('Cumulative Deformation (mm)')
+ax.set_ylabel('Energy (kNm)')
+plt.minorticks_on()
+ax.grid(which='major', color='grey', linewidth=0.5, alpha = 0.8)
+ax.grid(b=True, which='minor', linewidth=0.5, alpha = 0.4)
+
+
+
+
+
+
