1bb07dd175fd18ca3f94f86df7004c705ceacbd2
cslotboom <50510141+cslotboom@users.noreply.github.com>
2020-10-10 16:04:01 -0700

    hysteresis to hys
    rename hysteresis to hys

---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+# __all__ = ["hysteresis", "data"]
+
+# import hysteresis
+# from hysteresis.hysteresis import *
+# from hysteresis.data import *
+
+# import hysteresis.hysteresis
+# __all__ = ["hysteresis"]
