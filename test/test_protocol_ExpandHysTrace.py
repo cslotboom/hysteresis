2d119b05e98af34f1c209c6d4fdcda50ee3df059
cslotboom <50510141+cslotboom@users.noreply.github.com>
2021-01-02 04:30:47 -0800

    Update test_protocol_ExpandHysTrace.py

--- test/test_protocol_ExpandHysTrace.py
+++ test/test_protocol_ExpandHysTrace.py
@@ -22,26 +22,26 @@ def test_expandHysTrace():
     assert len(expand.Cycles) == 6
     
     
-curve = makeCurveBase()
-curve.plot()
-curve.plotLoadProtocol()
-fig, ax = curve.plotCycles()
-lines = fig.axes[0].lines
-for line in lines:
-    plt.setp(line, linestyle='--')
+# curve = makeCurveBase()
+# curve.plot()
+# curve.plotLoadProtocol()
+# fig, ax = curve.plotCycles()
+# lines = fig.axes[0].lines
+# for line in lines:
+#     plt.setp(line, linestyle='--')
 
-print(curve.NCycles)
-# print(np.round(curve.loadProtocol))
+# print(curve.NCycles)
+# # print(np.round(curve.loadProtocol))
 
 
 
-expand = hys.exandHysTrace(curve, [2])
-expand.plot()
-expand.plotLoadProtocol()
-fig, ax = curve.plotCycles()
-lines = fig.axes[0].lines
-for line in lines:
-    plt.setp(line, linestyle='--')
+# expand = hys.exandHysTrace(curve, [2])
+# expand.plot()
+# expand.plotLoadProtocol()
+# fig, ax = curve.plotCycles()
+# lines = fig.axes[0].lines
+# for line in lines:
+#     plt.setp(line, linestyle='--')
     
 
 
