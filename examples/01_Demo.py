cdbb2b06b512fb412d810fbff9bbee2756250080
cslotboom <50510141+cslotboom@users.noreply.github.com>
2021-01-02 01:15:57 -0800

    Updated gif to match proper syntax

--- examples/01_Demo.py
+++ examples/01_Demo.py
@@ -11,16 +11,6 @@ xy = np.column_stack([x,y])
 
 myHys = hys.Hysteresis(xy)
 myHys.plot(plotCycles = True)
-samllHys = hys.resample(myHys, 10)
-samllHys.plot()
-
-out = hys.compareHys(samllHys,myHys)
-
-
-
-
-
-
 
 
 
