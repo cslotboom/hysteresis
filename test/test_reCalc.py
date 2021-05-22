29c5c9c98b45bb848d81768dfcda23474e836dc8
cslotboom <50510141+cslotboom@users.noreply.github.com>
2021-05-22 00:57:54 -0700

    Updating Tests

--- test/test_reCalc.py
+++ test/test_reCalc.py
@@ -5,22 +5,42 @@ Created on Sat May  1 23:49:09 2021
 @author: Christian
 """
 
-
 import numpy as np
 import hysteresis as hys
+from  scipy.signal import sawtooth
 
+t = np.linspace(0,5*np.pi,1001)
+y = np.sin(t)
+xy = np.column_stack([t,y])
+
+# triangleSmall = sawtooth(t *20,0.5)/7
+# trianglexy = np.column_stack((triangleSmall, t))
+
+# myHys = hys.Hysteresis(trianglexy)
+# myHys.setLength()
+# myHys.recalculateCycles_dist()
+# myHys.plot(True)
+# myHys.re
+
+def test_recalc():
+    newHys = hys.SimpleCycle(xy)
+    newHys.setPeaks()
+    newHys.recalculatePeaks(peakProminence=3)
+    assert len(newHys.peakIndexes) == 2
+
+
+def test_recalc_dist():
+    
+    newHys = hys.SimpleCycle(xy)
+    newHys.setPeaks()
+    newHys.recalculatePeaks(peakProminence=3)
+    assert len(newHys.peakIndexes) == 2
 
 def test_recalc_like():
     revDist =2
     revWidth = 10
     revProminence = 50
-    
-    
-    t = np.linspace(0,1*np.pi,101)
-    y = np.sin(t)
-    xy = np.column_stack([t,y])
-    
-    
+        
     myHys = hys.Hysteresis(xy,revDist,revWidth,revProminence)
     
     newHys = hys.Hysteresis(xy)
@@ -34,6 +54,8 @@ def test_recalc_like():
     
     assert np.all([check1, check2, check3])
 
+
+test_recalc_like()
 # print(myHys.peakProminence)
 
 # out = myHys + t
