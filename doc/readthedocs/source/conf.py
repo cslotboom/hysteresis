bff97f357a62e8368b6d1d1c14e6586805286360
cslotboom <50510141+cslotboom@users.noreply.github.com>
2021-07-10 14:03:50 -0700

    Update conf.py

--- doc/readthedocs/source/conf.py
+++ doc/readthedocs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, CSlotboom'
 author = 'CSlotboom'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.8'
 
 
 # -- General configuration ---------------------------------------------------
