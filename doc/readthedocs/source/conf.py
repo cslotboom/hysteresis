4e9b8b52ab217fcfc6bd25c72aec4305dbb4b471
cslotboom <50510141+cslotboom@users.noreply.github.com>
2021-07-10 16:35:23 -0700

    Trying to remove lines in website example

--- doc/readthedocs/source/conf.py
+++ doc/readthedocs/source/conf.py
@@ -31,8 +31,11 @@ release = '1.0.8'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon', 'sphinx.ext.autosummary'
-             ]
+extensions = ['sphinx.ext.autodoc', 
+              'sphinx.ext.coverage', 
+              'sphinx.ext.napoleon', 
+              'sphinx.ext.autosummary'
+              'sphinx.ext.viewcode']
 autosummary_generate = True  # Turn on sphinx.ext.autosummary
 
 # Add any paths that contain templates here, relative to this directory.
